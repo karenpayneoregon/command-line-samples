9c89d5c41aad59de8d07d1477c44f14b640ceae8
Karen Payne <paynekaren@comcast.net>
2025-01-15 04:25:15 -0800

    refactor: simplify namespace declaration and clean up code structure

--- PromptForUserNamePassword/Classes/WhatEveryOneShows.cs
+++ PromptForUserNamePassword/Classes/WhatEveryOneShows.cs
@@ -1,26 +1,23 @@
 ﻿using System.Collections.Generic;
 using CommandLine;
 
-namespace PromptForUserNamePassword.Classes
+namespace PromptForUserNamePassword.Classes;
+
+public class WhatEveryOneShows
 {
-    public class WhatEveryOneShows
-    {
-        [Option('r', "read", Required = false, HelpText = "Input files to be processed.")]
-        public IEnumerable<string> InputFiles { get; set; }
+    [Option('r', "read", Required = false, HelpText = "Input files to be processed.")]
+    public IEnumerable<string> InputFiles { get; set; }
 
-        // Omitting long name, defaults to name of property, ie "--verbose"
-        [Option(Default = false, HelpText = "Prints all messages to standard output.")]
-        public bool Verbose { get; set; }
+    // Omitting long name, defaults to name of property, ie "--verbose"
+    [Option(Default = false, HelpText = "Prints all messages to standard output.")]
+    public bool Verbose { get; set; }
 
-        [Option("stdin", Default = false, HelpText = "Read from stdin")]
-        public bool stdin { get; set; }
+    [Option("stdin", Default = false, HelpText = "Read from stdin")]
+    public bool stdin { get; set; }
 
-        [Value(0, MetaName = "offset", HelpText = "File offset.")]
-        public long? Offset { get; set; }
+    [Value(0, MetaName = "offset", HelpText = "File offset.")]
+    public long? Offset { get; set; }
 
-        [Option('c', "concurrent", Required = false, HelpText = "Number of concurrent requests", Default = 1)]
-        public int ConcurrentCount { get; set; }
-
-
-    }
+    [Option('c', "concurrent", Required = false, HelpText = "Number of concurrent requests", Default = 1)]
+    public int ConcurrentCount { get; set; }
 }
